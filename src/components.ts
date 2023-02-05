cd8fb9a1903f615b49abbc427d5646b883e5d036
Issi <issi.dev.iobroker@gmail.com>
2023-02-06 00:19:15 +0100

    fixed Loader and add 3 Inputs (ip/password/number)

--- src/components.ts
+++ src/components.ts
@@ -3,12 +3,22 @@ export {
 	DropdownOption,
 	DropdownProps,
 } from "./lib/components/Dropdown";
+export {
+	IpAddressInput,
+	IpAddressInputProps,
+} from "./lib/components/IpAddressInput";
 export { Loader, LoaderProps } from "./lib/components/Loader";
 export {
 	ModalDialog,
 	ModalDialogProps,
 	ModalState,
 } from "./lib/components/ModalDialog";
+export { NumberInput, NumberInputProps } from "./lib/components/NumberInput";
+export {
+	PasswordColors,
+	PasswordInput,
+	PasswordInputProps,
+} from "./lib/components/PasswordInput";
 export {
 	SettingsHeader,
 	SettingsHeaderProps,
